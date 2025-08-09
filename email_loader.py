8217564dd99d42a0b573e23461aefd3f426006ec
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-09 01:09:16 -0700

    built tests for loader

--- email_loader.py
+++ email_loader.py
@@ -1,19 +1,30 @@
-import imapclient, os
+import imapclient, imaplib, os, pprint, pyzmail
 from datetime import date
-import pprint
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 email = os.getenv("EMAIL")
 app_password = os.getenv("APP_PASSWORD")
+
+imaplib._MAXLINE = 10000000
 
 imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
 imapObj.login(email, app_password)
 
-#pprint.pprint(imapObj.list_folders())
 imapObj.select_folder("INBOX", readonly=True)
 
-UIDs = imapObj.search([u'SINCE', date(2025, 8, 1), "UNSEEN"])
-print(UIDs)
+UIDs = imapObj.search([u'SINCE', date(2025, 8, 8), "UNSEEN"])
+#print(UIDs)
+
+raw_messages = imapObj.fetch(UIDs, ['BODY[]'])
+#pprint.pprint(raw_messages[23868])
+#print(raw_messages[23868][b"BODY"])
+
+
+messages = []
+for i in range(2):
+    messages.append(pyzmail.PyzMessage.factory(raw_messages[23868+i][b'BODY[]']))
+
+print(messages[0].get_subject())
+print(messages[0].text_part != None)
+print(messages[0].text_part.get_payload().decode("utf-8"))
