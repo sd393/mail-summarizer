fda6710c99b001e53ca16af3a31862977c75dc0c
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-08 20:34:13 -0700

    initial commit

---
+++ email_sender.py
@@ -0,0 +1,17 @@
+import os
+import smtplib
+from dotenv import load_dotenv
+
+load_dotenv()
+email = os.getenv("EMAIL")
+app_password = os.getenv("APP_PASSWORD")
+
+smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
+smtpObj.ehlo()
+print(smtpObj.starttls())
+
+smtpObj.login(email, app_password)
+
+smtpObj.sendmail(email, email, "Subject: testEmail \nHere's a summary of your recent emails!")
+
+smtpObj.quit()
