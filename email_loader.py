acfa9b583ad7142506bc420952106619f5c74ebf
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-11 15:07:46 -0700

    format emails

--- email_loader.py
+++ email_loader.py
@@ -6,30 +6,62 @@ load_dotenv()
 email = os.getenv("EMAIL")
 app_password = os.getenv("APP_PASSWORD")
 
-imaplib._MAXLINE = 10000000
+def get_recent_emails():
+    imaplib._MAXLINE = 10000000
 
-imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
-imapObj.login(email, app_password)
+    imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
+    imapObj.login(email, app_password)
 
-imapObj.select_folder("INBOX", readonly=True)
+    imapObj.select_folder("INBOX", readonly=True)
 
-UIDs = imapObj.search([u'SINCE', date(2025, 8, 10), "UNSEEN"])
-print(UIDs)
+    UIDs = imapObj.search([u'SINCE', date(2025, 8, 10), "UNSEEN"])
+    print(UIDs)
 
-raw_messages = imapObj.fetch(UIDs, ['BODY[]'])
-#pprint.pprint(raw_messages[23868])
-#print(raw_messages[23868][b"BODY"])
+    raw_messages = imapObj.fetch(UIDs, ['BODY[]'])
+    #pprint.pprint(raw_messages[23868])
+    #print(raw_messages[23868][b"BODY"])
 
 
 
-messages = []
-for i in range(2):
-    messages.append(pyzmail.PyzMessage.factory(raw_messages[23909+i][b'BODY[]']))
+    messages = []
+    for i in range(len(UIDs)):
 
+        total_message_info = pyzmail.PyzMessage.factory(raw_messages[UIDs[i]][b'BODY[]'])
+
+        email_info = []
+        email_info.append(total_message_info.get_subject())
+        email_info.append("")
+
+        if (total_message_info.text_part != None):
+            email_info[1] += total_message_info.text_part.get_payload().decode("utf-8")
+        
+        if (total_message_info.html_part != None):
+            email_info[1] += total_message_info.html_part.get_payload().decode(total_message_info.html_part.charset)
+        
+        messages.append(email_info)
+
+    imapObj.logout()
+
+    return messages
+
+def format_email_info():
+    message = ""
+
+    email_list = get_recent_emails()
+    for email in email_list:
+        message += "EMAIL:\n"
+        message += "SUBJECT: " + email[0] + "\n"
+        message += "BODY: " + email[1] + "\n"
+        message += "END OF EMAIL\n"
+
+    return message
+
+
+""" 
 print(messages[0].get_subject())
 print(messages[0].text_part != None)
 print(messages[0].text_part.get_payload().decode("utf-8"))
 print(messages[0].html_part.get_payload().decode(messages[0].html_part.charset))
+"""
 
-
-imapObj.logout()
+print(format_email_info())
