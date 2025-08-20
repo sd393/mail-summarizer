7d83beff1c0f225eeeab04f723dc19b96719df0e
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-19 23:57:29 -0700

    html support

--- email_sender.py
+++ email_sender.py
@@ -2,22 +2,40 @@ import os
 import smtplib
 from dotenv import load_dotenv
 import markdown
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 load_dotenv()
 email = os.getenv("EMAIL")
 app_password = os.getenv("APP_PASSWORD")
 
 def send_email(message):
+    #Define message container
+    message_container = MIMEMultipart('alternative')
+    message_container['Subject'] = "Link"
+    message_container["From"] = email
+    message_container["To"] = email 
+
+    #Create text part of message
+    text_part = MIMEText(message, 'text')
+
+    #Create HTML part of message
     html_output = markdown.markdown(message)
-    message = message.encode("utf-8")
-    message = message.decode("utf-8")
+    html_part = MIMEText(html_output, 'html')
+
+    #Attach to container
+    message_container.attach(text_part)
+    message_container.attach(html_part)
+
+    #message = message.encode("utf-8")
+    #message = message.decode("utf-8")
     smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
     smtpObj.ehlo()
     smtpObj.starttls()
 
     smtpObj.login(email, app_password)
 
-    smtpObj.sendmail(email, email, ("Subject: This Week's Inbox Summary \n" + message).encode("utf-8"))
+    smtpObj.sendmail(email, email, message_container.as_string())
 
     smtpObj.quit()
 if __name__ == "__main__":
