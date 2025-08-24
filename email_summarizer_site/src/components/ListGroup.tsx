6a66d31d83afb3c9c2797392b528ba437bcd7628
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-23 21:31:43 -0700

    state checking

--- email_summarizer_site/src/components/ListGroup.tsx
+++ email_summarizer_site/src/components/ListGroup.tsx
@@ -1,26 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
-    let items = [
-        "London",
-        "New York",
-        "Paris",
-        "Tokyo"
-    ];
+  let items = ["London", "New York", "Paris", "Tokyo"];
+  let selectedIndex = 0;
 
-    //items = [];
+  useState
 
-    return (
-        <>
-            <h1>List</h1>
+  return (
+    <>
+      <h1>List</h1>
 
-            {items.length === 0 && <p>No Items Found</p>};
+      {items.length === 0 && <p>No Items Found</p>}
 
-            <ul className="list-group">
-                {items.map((item) => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
-        </>
-    );
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={ selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            key={item}
+            onClick={() => { selectedIndex = index;}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
