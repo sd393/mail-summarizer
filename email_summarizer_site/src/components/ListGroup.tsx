80a2b2c22c647127af50d990cdf3fc811aa23b44
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-08-23 20:45:40 -0700

    dynamic lists

--- email_summarizer_site/src/components/ListGroup.tsx
+++ email_summarizer_site/src/components/ListGroup.tsx
@@ -1,12 +1,25 @@
 function ListGroup() {
+    let items = [
+        "London",
+        "New York",
+        "Paris",
+        "Tokyo"
+    ];
+
+    //items = [];
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        <>
+            <h1>List</h1>
+
+            {items.length === 0 && <p>No Items Found</p>};
+
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
     );
 }
 
