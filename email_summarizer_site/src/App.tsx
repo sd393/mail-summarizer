48ac9f8c5dcc5f3ff290435188ef39d794f5d64a
Samarjit Deshmukh <samarjitd391@gmail.com>
2025-09-24 23:51:11 -0400

    built nav bar

--- email_summarizer_site/src/App.tsx
+++ email_summarizer_site/src/App.tsx
@@ -1,7 +1,10 @@
 import ListGroup from "./components/ListGroup";
-
+import NavBar from "./components/NavBar";
 function App() {
+  return <div> <NavBar/> </div>
   return <div> <ListGroup /> </div>
 }
 
 export default App;
+
+
